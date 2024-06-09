3996531e2e72154196f4fc9f31dad7f11a479f64
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-06-10 01:15:39 +0200

    day 28 done

--- day 28.R
+++ day 28.R
@@ -77,4 +77,59 @@ snakefill(1)
 
 
 
+#Encode Morse
+encode_morse <- function(x){
+  x <- toupper(x)
+  x.splited<-strsplit(x,"")[[1]]
+  y<- char_to_dots[x.splited]
+  paste0(y,collapse = " ") 
+}
+
+char_to_dots = c(
+  'A'= '.-', 'B'= '-...', 'C'= '-.-.', 'D'= '-..', 'E'= '.', 'F'= '..-.',
+  'G'= '--.', 'H'= '....', 'I'= '..', 'J'= '.---', 'K'= '-.-', 'L'= '.-..',
+  'M'= '--', 'N'= '-.', 'O'= '---', 'P'= '.--.', 'Q'= '--.-', 'R'= '.-.',
+  'S'= '...', 'T'= '-', 'U'= '..-', 'V'= '...-', 'W'= '.--', 'X'= '-..-',
+  'Y'= '-.--', 'Z'= '--..', ' '= ' ', '0'= '-----',
+  '1'= '.----', '2'= '..---', '3'= '...--', '4'= '....-', '5'= '.....',
+  '6'= '-....', '7'= '--...', '8'= '---..', '9'= '----.',
+  '&'= '.-...', "'"= '.----.', '@'= '.--.-.', ')'= '-.--.-', '('= '-.--.',
+  ':'= '---...', ','= '--..--', '='= '-...-', '!'= '-.-.--', '.'= '.-.-.-',
+  '-'= '-....-', '+'= '.-.-.', '"'= '.-..-.', '?'= '..--..', '/'= '-..-.'
+)
+
+
+#encode_morse("EDABBIT CHALLENGE") ➞ ". -.. .- -... -... .. -   -.-. .... .- .-.. .-.. . -. --. ."
+
+#encode_morse("HELP ME !") ➞ ".... . .-.. .--.   -- .   -.-.--"
+
+
+
+
+#Disarium Number
+
+is_disarium <- function(n){
+  n.splited <- strsplit(as.character(n),"")[[1]]
+  f.n <-as.numeric(n.splited)
+  result <- numeric(0)
+  
+  for (i in seq_len(length(n.splited))) {
+    result[[length(result) + 1]] <- f.n[[i]]^i
+    
+  }
+  sum(result) == n
+}
+# is_disarium(75) ➞ False
+# # 7^1 + 5^2 = 7 + 25 = 32
+# 
+# is_disarium(135) ➞ True
+# # 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+# 
+# is_disarium(544) ➞ False
+# 
+# is_disarium(518) ➞ True
+# 
+# is_disarium(466) ➞ False
+# 
+# is_disarium(8) ➞ True
 
