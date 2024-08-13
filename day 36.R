a685ef7af3dbfa125840b592d841377f7177e051
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-08-13 23:09:44 +0200

    more done

--- day 36.R
+++ day 36.R
@@ -32,7 +32,7 @@ sum(grepl("bad cookie",lst,perl = TRUE))
 
 
 parse_code <- function(x){
-  final<-c(regmatches(x,gregexec("(?<first_name>^[A-z][a-z]+)0+(?<last_name>[A-z][A-z]+)0+(?<id>[0-9]+)",x,perl = TRUE))[[1]])
+  final<-c(regmatches(x,gregexec("(?<first_name>^[A-z]+)0+(?<last_name>[A-z]+)0+(?<id>[0-9]+)",x,perl = TRUE))[[1]])
   
   fin <- c("first_name"= final[2],"last_name"= final[3],"id" = final[4])
   fin
@@ -57,5 +57,51 @@ parse_code <- function(x){
 #   "last_name": "LEE",
 #   "id": "43"
 # }
+parse_code('a0b01')
+
+
+
+does_rhyme <- function(x,y){
+  
+  
+  x.vect<-strsplit(x,"\\s+")[[1]]
+  m.1 <-regmatches(x.vect[length(x.vect)],gregexpr("[aeiou]",x.vect[length(x.vect)],perl = TRUE))[[1]]
+  
+  
+  y.vect<-strsplit(y,"\\s+")[[1]]
+  m.2 <- regmatches(y.vect[length(y.vect)],gregexpr("[aeiou]",y.vect[length(y.vect)],perl = TRUE))[[1]]
+  
+  all(m.1==m.2)
+}
+
+
+
+
+# 
+# does_rhyme("Sam I am!", "Green eggs and ham.") ➞ True
+# 
+# does_rhyme("Sam I am!", "Green eggs and HAM.") ➞ True
+# # Capitalization and punctuation should not matter.
+# 
+# does_rhyme("You are off to the races", "a splendid day.") ➞ False
+# 
+# does_rhyme("and frequently do?", "you gotta move.") ➞ False
+does_rhyme('You are off to the races', 'a splendid day.')
+does_rhyme('your elbow and chin!', 'how much can you win?')
+
+
+
+
+txt = "123 Redding Dr. 1560 Knoxville Ave. 3030 Norwalk Dr. 5 South St."
+# pattern = "yourregularexpressionhere"
+# 
+# (re.findall(pattern, txt)) ➞ ["123 Redding Dr.", "1560 Knoxville Ave.", "3030 Norwalk Dr.", "5 South St."]
+
+m <-regmatches(txt,gregexpr("[0-9]+ *[A-z]+ *[A-z]+.",txt,perl = TRUE))[[1]]
+m<-trimws(m)
+m
+
+
+
 
 
