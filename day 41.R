1fb6a07e602202b1f6886c57c4e3080ec32188a2
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-08-31 23:22:38 +0200

    more done

--- day 41.R
+++ day 41.R
@@ -26,3 +26,51 @@ sort_by_character <- function(x,y){
 
 
 
+
+sort_by_last <- function(x){
+  
+  k<-strsplit(x,"\\s+")[[1]]
+    result<- character(0)
+  
+  substr(k[[1]],nchar(k[[1]]),nchar(k[[1]]))
+  result<- character(0)
+  
+  for (i in seq_len(length(k))) {
+    result<-c(result, substr(k[[i]],nchar(k[[i]]),nchar(k[[i]])))
+    
+    
+  }
+  final <-k[order(result)]
+  paste0(final,collapse = " ")
+}
+
+
+# sort_by_last("herb camera dynamic") ➞ "camera herb dynamic"
+# 
+# sort_by_last("stab traction artist approach") ➞ "stab approach traction artist"
+# 
+# sort_by_last("sample partner autonomy swallow trend") ➞ "trend sample partner swallow autonomy"
+
+
+
+
+make_grlex <- function(x){
+  
+  k<-vapply(x,function(k){
+    nchar(k)
+  },numeric(1))
+  
+  y<-names(sort(k))
+  p<-sort_by_character(y,1)
+  return(p)
+}
+
+
+# make_grlex(c("small", "big")) ➞ ["big", "small"]
+# 
+# make_grlex(c("cat", "ran", "for", "the", "rat")) ➞ ["cat", "for", "ran", "rat", "the"]
+# 
+# make_grlex(c("this", "is", "a", "small", "test")) ➞ ["a", "is", "small","this", "test"]
+
+
+
