e8f60210ebd3dcbab431f34aa2ae3737f0194480
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-09-01 00:00:41 +0200

    more done

--- day 41.R
+++ day 41.R
@@ -74,3 +74,59 @@ make_grlex <- function(x){
 
 
 
+
+
+
+
+str_to_dict <- function(x){
+  
+  k<-lapply(x, function(k){
+    strsplit(k,"=")[[1]]
+  })
+  
+  
+  result<- character(0)
+  result.1<- character(0)
+  
+  
+  for (i in seq_len(length(k))) {
+    result<-c(result,k[[i]][[1]])
+    result.1<-c(result.1,k[[i]][2])
+  }
+  
+  result
+  result.1
+  
+  names(result.1)<-result
+  
+  result.1
+  
+  
+}
+
+
+# str_to_dict(c("1=one", "2=two", "3=three", "4=four")) ➞ {"1": "one", "2": "two", "3": "three", "4": "four"}
+# 
+# str_to_dict(c("dog=bark", "cat=meow", "cow=moo")) ➞ {"dog": "bark", "cat": "meow", "cow": "moo"}
+# 
+# str_to_dict(c("bob=human", "lola=dog", "mittens=cat", "todd=frog")) ➞ {"bob": "human", "lola": "dog", "mittens": "cat", "todd": "frog"}
+
+
+
+move_zeros <- function(x){
+  
+  k<-sub("0",NA,x)
+  new.k<-k[!is.na(k)]
+  c(new.k,rep(0,sum(is.na(k))))
+}
+
+
+
+
+# move_zeros(c(1, 0, 1, 2, 0, 1, 3)) ➞ [1, 1, 2, 1, 3, 0, 0]
+# 
+# move_zeros(c(0, 1, NULL, 2, FALSE, 1, 0)) ➞ [1, None, 2, false, 1, 0, 0]
+# 
+# move_zeros(c('a', 0, 0, 'b', 'c', 'd', 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9)) ➞ ['a', 'b', 'c', 'd', 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
