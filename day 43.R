305063f6128e1e9535c73124351f335b8179d758
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-09-29 23:40:53 +0200

    4 exercise done on .SD

--- day 43.R
+++ day 43.R
@@ -23,6 +23,7 @@ dt[, .(mean_income = mean(income), income_range = max(income) - min(income)), by
 
 # Exercises
 
+#dt[i,j,by]
 
 # Sample data.table
 dt <- data.table(
@@ -47,3 +48,29 @@ dt[,.N,by = .(city)]
 dt[,.(range_of_income = max(income) - min(income)) ,by = .(name)]
 
 
+###SD
+
+
+dt <- data.table(
+  name = c("John", "Alice", "Bob", "John", "Alice", "Bob", "John", "Alice"),
+  age = c(30, 25, 28, 31, 26, 29, 32, 27),
+  income = c(45000, 55000, 30000, 48000, 56000, 32000, 50000, 57000),
+  city = c("NY", "LA", "NY", "LA", "NY", "LA", "NY", "LA")
+)
+
+
+dt[,lapply(.SD,mean),by = city,.SDcols = c("age","income")]
+
+dt[,.(mean_income = mean(income), sum_income = sum(income)),by = .(name),.SDcols = c("income")]
+
+dt[, lapply(.SD, function(x) list(mean = mean(x), sum = sum(x))), by = name, .SDcols = "income"]
+
+
+dt[,lapply(.SD, function(x) list(sum_age = sum(age),sum_income. = sum(income))),by = .(city),.SDcols = c("age","income")]
+
+dt[, lapply(.SD, sum), by = city, .SDcols = c("age", "income")]
+
+dt[,lapply(.SD,function(x) (x -mean(x))/sd(x)),by = .(name), .SDcols = c("age","income")]
+
+
+dt[, lapply(.SD, function(x) (x - mean(x)) / sd(x)), by = name, .SDcols = c("age", "income")]
