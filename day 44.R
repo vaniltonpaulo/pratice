5b4ed88ac025dd5dec830873e9be74dc4c8fb44f
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-01 23:22:39 +0200

    more done sucessfully

--- day 44.R
+++ day 44.R
@@ -62,3 +62,78 @@ sales[,.N,by = .(channel,book)][channel == "online",.(book)]
 
 #Exercise 8
 prices[sales,on = "book"][,.(avg_mean =mean(ifelse(channel == "online", price.online, price.onsite))), by =.(channel)]
+
+
+
+################################## baby step
+
+customers <- data.table(
+  name = c("Alice", "Bob", "Charlie", "David", "Eve"),
+  age = c(25, 40, 35, 28, 50),
+  city = c("New York", "Los Angeles", "Chicago", "Miami", "Boston")
+)
+
+#Exercise 1.1
+customers[age>30,]
+
+
+
+customers[,age_squared:= age ^2 ][]
+
+
+sales <- data.table(
+  product = c("Laptop", "Mouse", "Laptop", "Mouse", "Keyboard"),
+  quantity = c(2, 1, 1, 3, 2)
+)
+
+sales[ , .(quantity = sum(quantity)),by = product]
+
+
+
+products <- data.table(
+  product = c("Laptop", "Mouse", "Keyboard"),
+  price = c(1000, 50, 150)
+)
+
+sales <- data.table(
+  product = c("Laptop", "Mouse", "Keyboard", "Laptop", "Mouse"),
+  quantity = c(2, 1, 3, 1, 2)
+)
+
+sales[products,on  ="product"][,.(rev = sum(quantity * price)),by=product]
+
+
+transactions <- data.table(
+  date = as.Date(c("2024-01-15", "2024-01-20", "2024-02-10", "2024-02-20")),
+  amount = c(200, 150, 300, 250)
+)
+
+transactions[,sum(amount),by = month(date)]
+
+
+
+sales <- data.table(
+  product = c("Laptop", "Mouse", "Keyboard", "Laptop", "Mouse"),
+  quantity = c(12, 9, 15, 4, 8)
+)
+
+sales[,product_rating := ifelse(quantity > 10,"Top Seller","Regular Seller")][]
+
+sales <- data.table(
+  product = c("Laptop", "Mouse", "Keyboard", "Laptop", "Mouse"),
+  onsite_quantity = c(2, 1, 0, 1, 3),
+  online_quantity = c(3, 2, 1, 2, 0)
+)
+
+sales[,.(quantity = sum(onsite_quantity+online_quantity)),by = product]
+
+
+
+customers <- data.table(
+  name = c("Alice", "Bob", "Charlie", "David", "Eve"),
+  age = c(25, NA, 35, 28, NA)
+)
+mean_age <- customers[, mean(age, na.rm = TRUE)]
+
+# Step 2: Replace missing age values (NA) with the calculated mean
+customers[is.na(age), age := mean_age]
