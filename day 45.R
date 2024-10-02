9875d67a89292c0767e7109dc1ca1d2e9ec4e15c
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-02 21:57:12 +0200

    exercise 1 done

--- day 45.R
+++ day 45.R
@@ -8,3 +8,17 @@ sales <- data.table(
   item = c("Potion", "Sword", "Potion", "Armor", "Shield", "Sword", "Sword"),
   channel = c("onsite", "online", "online", "onsite", "onsite", "onsite", "online")
 )
+
+#exercise 1.1
+sales[,.(count = .N),by=.(item)]
+
+#exercise 1.3
+
+x<-dcast(sales,item~channel,fun.aggregate = length)
+setnames(x,c("online","onsite"),c("count_online","count_onsite"))
+x
+
+
+#exercise 1.3
+
+sales[prices,on = "item"][,sum(ifelse(channel == "online",price_onsite * 0.9,price_online * 0.95))][]
