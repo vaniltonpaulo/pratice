df55a975d643a03a1484e17e6b5230d4acf94524
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-03 19:16:23 +0200

    more stuff

--- data table cheatsheet.R
+++ data table cheatsheet.R
@@ -4,3 +4,8 @@ flights.data[][order(number_of_fligth, decreasing = TRUE)]
 #how to match multiple values in a row
 
 flights.data[carrier %in%  carrier.code, , ]
+
+
+#keep the collumns and rows exact the same in the left table  and making the table on the rigth match according to that
+
+merge(first, second, by = c(z ="month",x ="origin", y ="dest"), all.x = TRUE)
