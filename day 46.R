01197540bc54587ef733d28adb890bf8eb592d54
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-04 16:06:26 +0200

    Advanced done

--- day 46.R
+++ day 46.R
@@ -131,3 +131,36 @@ salary_data[YearsAtCompany > 5, .(avg.salary = mean(Salary))]
 
 #What is the salary range (min, max) in each department?
 salary_data[,.(min.salary = min(Salary),max.salary = max(Salary)),by =Department ]
+
+
+
+#Advanced Questions
+
+#What is the median salary for employees who are under 35 and have been with the company for more than 3 years?
+
+salary_data[YearsAtCompany > 3 & Age <35,.(median.salary = as.numeric(median(Salary)))]
+  
+
+
+#Calculate the ratio of male to female employees in each department. Which department has the most gender imbalance?
+
+gender_count_by_department <- salary_data[,.(Count = .N),by=.(Department,Gender)]
+
+gender_count_wide <- dcast(gender_count_by_department, Department ~ Gender, value.var = "Count", fill = 0)
+
+gender_count_wide[,.(ratio = M/F)]
+
+
+#Which employee(s) have a salary above the department’s average for their respective department?
+salary_data[,avg.salary:=mean(Salary),by = Department][Salary >avg.salary][,Name]
+
+
+
+#For employees who have been with the company for 5 years or less, what is the average salary by 
+#gender, and how does it compare to employees who have been with the company for more than 5 years?
+
+
+result<-salary_data[YearsAtCompany < 5,.(avg.five.less = mean(Salary)),by = Gender]
+result.2 <-salary_data[YearsAtCompany > 5,.(avg.five.more = mean(Salary)),by = Gender]
+merge(result,result.2,by = "Gender",all.x = TRUE)
+
