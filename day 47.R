6461adf79aeb71d3101e1113f897d15fa41cc77c
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-06 00:55:27 +0200

    finished very easy, easy and start medium

--- day 47.R
+++ day 47.R
@@ -254,15 +254,196 @@ n_tables_plus_one <- function(x){
 # 
 # n_tables_plus_one(3) ➞ "4,7,10,13,16,19,22,25,28,31"
 
-
+middle_earth <- function(x){
+  if( which(x == "Frodo")+ 1 ==  which(x == "Sam")) return(TRUE)
+  if( which(x == "Sam")+ 1 ==  which(x == "Frodo")) return(TRUE)
+  FALSE
+  
+}
 
 
 # middle_earth(c("Frodo", "Sam", "Gandalf")) ➞ True
 # 
 # middle_earth(c("Frodo", "Saruman", "Sam")) ➞ False
 # 
-# middle_earth(c("Orc", "Sam", "Frodo", "Legolas") ➞ True
+# middle_earth(c("Orc", "Sam", "Frodo", "Legolas")) ➞ True
 
 
-x <- c("Frodo", "Sam", "Gandalf")
+
+
+is_omnipresent <- function(x,y){
+  result<-lapply(x, function(k){
+    y %in% k
+  })
+  
+  all(unlist(result))
+}
+
+
+
+
+# is_omnipresent(list(c(1, 1), c(1, 3), c(5, 1), c(6, 1)), 1) ➞ True
+# 
+# is_omnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6) ➞ False
+# 
+# is_omnipresent([[5], [5], [5], [6, 5]], 5) ➞ True
+# 
+# is_omnipresent(list(c(5), c(5), c(5), c(6, 5)), 6) ➞ False
+
+
+
+####Easy
+
+stutter <- function(x){
+  first.two<-substr(x,1,2)
+  paste0(first.two,"...",first.two,"...",x,"?")
+}
+# stutter("incredible") ➞ "in... in... incredible?"
+# 
+# stutter("enthusiastic") ➞ "en... en... enthusiastic?"
+# 
+# stutter("outstanding") ➞ "ou... ou... outstanding?"
+
+
+solve_for_exp <- function(x,y){
+  result<- numeric(0)
+  
+  for (i in seq_len(y)) {
+    if((x^i) == y){
+      result[[length(result) +1 ]] <- i
+    }
+    
+  }
+  result
+}
+
+# solve_for_exp(4, 1024) ➞ 5
+# 
+# solve_for_exp(2, 1024) ➞ 10
+# 
+# solve_for_exp(9, 3486784401) ➞ 10
+
+
+
+missing_num <- function(x){
+  x<-sort(x)
+  y<-seq_len(10)
+  result<- numeric(0)
+  
+  for (i in seq_len(10)) {
+    if(  all(sort(append(x,i)) == y)){
+      result[[length(result) +1 ]] <- i
+      
+    }
+  }
+  
+  result
+}
+
+
+
+# missing_num(c(1, 2, 3, 4, 6, 7, 8, 9, 10)) ➞ 5
+# 
+# missing_num(c(7, 2, 3, 6, 5, 9, 1, 4, 8)) ➞ 10
+# 
+# missing_num(c(10, 5, 1, 2, 4, 6, 8, 3, 9)) ➞ 7
+
+
+
+square_digits <- function(x){
+  x <- as.character(x)
+  
+  result<-strsplit(x,"")[[1]]
+  result<-as.numeric(result)
+  
+  final<-result ^2
+  
+  as.numeric(paste0(final,collapse = ""))
+  
+}
+
+
+
+# square_digits(9119) ➞ 811181
+# 
+# square_digits(2483) ➞ 416649
+# 
+# square_digits(3212) ➞ 9414
+
+
+paths <- function(n){
+  factorial(n)
+}
+
+# paths(4) ➞ 24
+# 
+# paths(1) ➞ 1
+# 
+# paths(9) ➞ 362880
+
+
+century_from_year <- function(x){
+  if(between(x,1801 , 1900)) return(19)
+  if(between(x,1901, 2000)) return(20)
+  return(21)
+  
+}
+
+# century_from_year(2005) ➞ 21
+# 
+# century_from_year(1950) ➞ 20
+# 
+# century_from_year(1900) ➞ 19
+
+
+get_discounts <- function(x,y){
+  num<-regmatches(y,gregexpr("[0-9]+",y,perl = TRUE))[[1]]
+  num <- as.numeric(num)
+  
+  percentange<-num/100
+  x * percentange
+}
+
+
+# get_discounts(c(2, 4, 6, 11), "50%") ➞ [1, 2, 3, 5.5]
+# 
+# get_discounts(c(10, 20, 40, 80), "75%") ➞ [7.5, 15, 30, 60]
+# 
+# get_discounts(c(100), "45%") ➞ [45]
+
+
+
+even_or_odd <- function(x){
+  result <- strsplit(x,"")[[1]]
+  result <- as.numeric(result)
+  even.num <- sum(result[result %% 2 == 0])
+  odd.num <- sum(result[result %% 2 == 1])
+  
+  if(even.num > odd.num) return("Even is greater than Odd")
+  if(even.num < odd.num) return("Odd is greater than Even")
+  if(even.num == odd.num) return("Even and Odd are the same")
+}
+
+# even_or_odd("22471") ➞ "Even and Odd are the same"
+# 
+# even_or_odd("213613") ➞ "Even and Odd are the same"
+# 
+# even_or_odd("23456") ➞ "Even is greater than Odd"
+
+
+x <-"22471"
+
+result <- strsplit(x,"")[[1]]
+result <- as.numeric(result)
+even.num <- result[result %% 2 == 0]
+odd.num <- result[result %% 2 == 1]
+
+
+
+############# Medium start
+
+
+
+
+
 
