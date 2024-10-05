e9afc87d8bccb3cf6f7c411efdef458c7962756c
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-05 12:26:50 +0200

    Before Breakfast aLGOS

---
+++ day 47.R
@@ -0,0 +1,162 @@
+next_edge <- function(x,y){
+  result <- (x+y) -1
+  result
+}
+
+# next_edge(8, 10) ➞ 17
+# 
+# next_edge(5, 7) ➞ 11
+# 
+# next_edge(9, 2) ➞ 10
+
+
+
+animals <- function(x,y,z){
+  
+  chickens <- x *2
+  cows <- y * 4
+  pigs <- z * 4
+  
+  sum(chickens,cows,pigs)
+}
+
+
+# animals(2, 3, 5) ➞ 36
+# 
+# animals(1, 2, 3) ➞ 22
+# 
+# animals(5, 2, 8) ➞ 50
+
+
+
+makes10 <- function(a,b){
+  if( a == 10 || b == 10 ) return(TRUE)
+  if(sum(a,b) == 10) return(TRUE)
+  
+  FALSE
+}
+
+# makes10(9, 10) ➞ True
+# 
+# makes10(9, 9) ➞ False
+# 
+# makes10(1, 9) ➞ True
+
+
+
+frames <- function(x,y){
+  frame_amount <- x * 60
+  result <- frame_amount * y
+  result
+}
+
+
+# frames(1, 1) ➞ 60
+# 
+# frames(10, 1) ➞ 600
+# 
+# frames(10, 25) ➞ 15000
+frames(52, 33)
+
+
+even_or_odd <- function(...){
+  if(missing(...)) return("even")
+ifelse(  sum(unlist(list(...))) %% 2 == 0,"even","odd")
+  }
+
+# even_or_odd(0) ➞ "even"
+# 
+# even_or_odd(1) ➞ "odd"
+# 
+# even_or_odd() ➞ "even"
+# 
+# even_or_odd(0, 1, 5) ➞ "even"
+
+
+
+area_shape <- function(x,y,z){
+  ifelse(z == "triangle",0.5 *x*y, x *y)
+}
+
+
+# area_shape(2, 3, "triangle") ➞ 3
+# 
+# area_shape(8, 6, "parallelogram") ➞ 48
+# 
+# area_shape(2.9, 1.3, "parallelogram") ➞ 3.77
+
+
+future_people <- function(currentPopulation, birthsPerMonth){
+  threeDecades <-3*10
+  months.in.decade <-decades * 12
+  
+  birthsInThreeDecades <-months.in.decade *birthsPerMonth
+  futurePop <-birthsInThreeDecades + currentPopulation
+  futurePop
+}
+
+
+# future_people(256, 2) ➞ 976
+# 
+# future_people(3248, 6) ➞ 5408
+# 
+# future_people(5240, 3) ➞ 6320
+
+count_d <- function(x){
+  result<-strsplit(x,"")[[1]]
+  
+  final<-regmatches(x,gregexpr("d|D",x,perl = TRUE))[[1]]
+  sum(nchar(final))
+}
+
+# count_d("My friend Dylan got distracted in school.") ➞ 4
+# 
+# count_d("Debris was scattered all over the yard.") ➞ 3
+# 
+# count_d("The rodents hibernated in their den.") ➞ 3
+
+
+
+accept_into_movie <- function(x,y){
+  if(x >= 15) return(TRUE)
+  ifelse(x < 15 & y == TRUE,TRUE,FALSE)
+  
+}
+
+# accept_into_movie(14, TRUE) ➞ True
+# 
+# accept_into_movie(14, FALSE) ➞ False
+# 
+# accept_into_movie(16, FALSE) ➞ True
+
+
+stack_boxes <- function(x){
+  if( x == 1) return(1)
+  if( x == 0) return(0)
+  
+  result <- x*x
+  result
+}
+
+
+# stack_boxes(1) ➞ 1
+# 
+# stack_boxes(2) ➞ 4
+# 
+# stack_boxes(0) ➞ 0
+stack_boxes(512)
+
+stack_boxes(27)
+
+
+
+programmers <- function(...){
+  x <- unlist(list(...))
+  max(x) -min(x)
+}
+
+# programmers(147, 33, 526) ➞ 493
+# 
+# programmers(33, 72, 74) ➞ 41
+# 
+# programmers(1, 5, 9) ➞ 8
