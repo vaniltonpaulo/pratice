8e3373f2675d7210b0e96d44c42ce67029c8b91a
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-13 18:30:24 +0200

    used stash

--- day50.R
+++ day50.R
@@ -4,4 +4,38 @@ stock_picker(c(10, 12, 4, 5, 9)) #➞ 5
 
 stock_picker(c(14, 20, 4, 12, 5, 11)) #➞ 8
 
+
+stock_picker(c(80, 60, 10, 8))# ➞ #-1
+
+
+
+
+stock_picker(c(10, 12, 4, 5, 9)) #➞ 5
+
+stock_picker(c(14, 20, 4, 12, 5, 11)) #➞ 8
+
+stock_picker(c(80, 60, 10, 8))# ➞ #-1
+
+stock_picker(c(10, 12, 4, 5, 9)) #➞ 5
+
+stock_picker(c(14, 20, 4, 12, 5, 11)) #➞ 8
+
+stock_picker(c(80, 60, 10, 8)) #➞ #-1
+
+
+stock_picker(c(10, 12, 4, 5, 9)) #➞ 5
+
+stock_picker(c(14, 20, 4, 12, 5, 11)) #➞ 8
+
+stock_picker(c(80, 60, 10, 8)) #➞ #-1
+
+stock_picker(c(10, 12, 4, 5, 9)) #➞ 5
+
+#hhidwhgiyvwy
+
+x<- "hidgywg"
+
+stock_picker(c(14, 20, 4, 12, 5, 11)) #➞ 8
+
+
 stock_picker(c(80, 60, 10, 8)) ➞ #-1
