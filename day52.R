44d6397d77b6d5f9b5488243a1c0a709ffea8420
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-10-17 21:51:40 +0200

    hey

--- day52.R
+++ day52.R
@@ -22,3 +22,62 @@ chunk_size<-nchar(x) / y
 
 startposition <- seq(1,nchar(x), by = chunk_size)
 substring(x,startposition, startposition + chunk_size -1)
+
+
+
+
+same_length <- function(x){
+  result <- strsplit(x,"")[[1]]
+  result <- as.numeric(result)
+  sum(result ==1) ==  sum(result ==0)
+}
+
+
+same_length <- function(x) {
+  # Split the input string into a vector of characters
+  result <- strsplit(x, "")[[1]]
+  
+  # Initialize counters
+  i <- 1
+  n <- length(result)
+  
+  while (i <= n) {
+    # Count the length of the current sequence of '1's
+    if (result[i] == "1") {
+      count_ones <- 0
+      while (i <= n && result[i] == "1") {
+        count_ones <- count_ones + 1
+        i <- i + 1
+      }
+      
+      # Count the length of the subsequent sequence of '0's
+      count_zeroes <- 0
+      while (i <= n && result[i] == "0") {
+        count_zeroes <- count_zeroes + 1
+        i <- i + 1
+      }
+      
+      # If the counts are not equal, return FALSE
+      if (count_ones != count_zeroes) {
+        return(FALSE)
+      }
+    } else {
+      # Move to the next character if it's not '1'
+      i <- i + 1
+    }
+  }
+  
+  # If all sequences matched, return TRUE
+  return(TRUE)
+}
+
+# 
+# same_length("110011100010") ➞ True
+# 
+# same_length("101010110") ➞ False
+# 
+# same_length("111100001100") ➞ True
+# 
+# same_length("111") ➞ False
+
+766
