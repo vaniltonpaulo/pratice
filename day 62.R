3d3aaa57c730da6ad283ac71a4428c50d23790c4
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-11-06 00:11:22 +0100

    strings done

--- day 62.R
+++ day 62.R
@@ -36,4 +36,47 @@ change_string("sNaKe")
 # 
 # add_str_nums("1874682736267235927359283579235789257", "32652983572985729") ➞ "1874682736267235927391936562808774986"
 
-#TOO EASY EVEN THOUGH ITS LABELED AS HARD
+#TOO EASY EVEN THOUGH ITS LABELED AS HARD 
+
+
+
+inverter <- function(x,y) {
+  if (y == "P") {
+    result<-strsplit(x," ")[[1]]
+    result<-rev(result)
+    
+    regmatches(result[[1]],gregexpr("^[A-z]",result[[1]],perl = TRUE))[[1]] <- toupper(substr(result[[1]],1,1))
+    
+    return(paste(result[[1]],paste(vapply(result[2:length(result)],tolower,character(1)),collapse = " "),collapse = " "))
+  } else if ( y == "W") {
+    result <- strsplit(x," ")[[1]]
+    
+    first<-strsplit(result[[1]],"")[[1]]
+    first<-rev(first)
+    first[[1]] <- toupper(first[[1]])
+    first.sample<-vapply(first[2:length(first)],tolower,character(1))
+    result[[1]] <-paste0(first[[1]],paste(first.sample,collapse = ""),collapse = "")
+    
+    
+    done<-vapply(result[2:length(result)],function(k){
+      so <- strsplit(k,"")[[1]]
+      so <- rev(so)
+      paste0(so,collapse = "")
+    },character(1))
+    
+    return(paste(result[[1]],paste(done,collapse = " "),collapse = ""))
+    
+  }
+  
+}
+
+
+
+
+# inverter("This is Valhalla", "P") ➞ "Valhalla is this"
+# 
+# inverter("One fine day to start", "W") ➞ "Eno enif yad ot trats"
+# 
+# inverter("Division by powers of two", "P") ➞ "Two of powers by division"
+
+inverter("Excellence is achievable", "W")
