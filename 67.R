63f7bf0893c79dbfe635135151ec270d7a9ae518
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-11-28 00:38:57 +0100

    you live to learn

--- 67.R
+++ 67.R
@@ -46,9 +46,71 @@ consecutive_combo <- function(x, y) {
 # 
 # consecutive_combo(c(44, 46), c(45)) ➞ True
 
-x <-c(7, 4, 5, 1)
-y <- c(2, 3, 6)
 
-result <- sort(union(x,y))
-final<-min(result):max(result)
-ifelse(isTRUE(all.equal(result,final)),TRUE,FALSE)
+
+tallest_skyscraper <- function(mat){
+  max(apply(mat, 2, sum))
+}
+grid1 <- matrix(c(
+  0, 0, 0, 0,
+  0, 1, 0, 0,
+  0, 1, 1, 0,
+  1, 1, 1, 1
+), nrow = 4, byrow = T)
+
+# Example 2
+grid2 <- matrix(c(0, 1, 0, 0,
+                  0, 1, 0, 0,
+                  0, 1, 1, 0,
+                  1, 1, 1, 1
+), nrow = 4, byrow = TRUE)
+
+# Example 3
+grid3 <- matrix(c(
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  1, 1, 1, 0,
+  1, 1, 1, 1
+), nrow = 4, byrow = TRUE)
+
+tallest_skyscraper(grid1)
+tallest_skyscraper(grid2)
+tallest_skyscraper(grid3)
+
+remove_letters <- function(x,y) {
+  res <- strsplit(y,"")[[1]]
+  setdiff(x,res)
+}
+
+
+remove_letters <- function(x,y){
+  res <- strsplit(y,"")[[1]]
+  
+  for (i in seq_len(length(res))) {
+    if(isTRUE(any(x[1] %in% res))) {
+      x <- x[-1]
+    }
+  }
+  x 
+}
+
+
+# remove_letters(c("s", "t", "r", "i", "n", "g", "w"), "string") ➞ ["w"]
+# 
+# remove_letters(c("b", "b", "l", "l", "g", "n", "o", "a", "w"), "balloon") ➞ ["b", "g", "w"]
+# 
+# remove_letters(c("d", "b", "t", "e", "a", "i"), "edabit") ➞ []
+remove_letters(c("t", "t", "e", "s", "t", "u"), "testing")
+
+lst <- c("t", "t", "e", "s", "t", "u")
+str <- "testing"
+str_split <- unlist(strsplit(str, ""))
+
+# Remove each letter in the string from the list
+for (char in str_split) {
+  match_index <- match(char, lst)
+  if (!is.na(match_index)) {
+    lst <- lst[-match_index]
+  }
+}
+match(str_split[[1]],lst)
