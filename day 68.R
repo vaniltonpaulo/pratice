3ecffd790e1d9f74ee8400a23d1ad6ff59f5ecf2
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-12-03 23:21:54 +0100

    2 done

--- day 68.R
+++ day 68.R
@@ -16,3 +16,26 @@ show_the_love <- function(x){
 # show_the_love(c(2, 100)) ➞ [27, 75]
 
 
+find_fulcrum <- function(x){
+  result <- numeric(0)
+  
+  for (i in seq_along(x)) {
+    if(isTRUE(all.equal(sum(x[1:(i - 1)]),sum(x[(i + 1):length(x)])))){
+      result[[length(result) + 1]] <- x[i]
+    }
+  }
+  
+  if(length(result) == 0) return(-1)
+  result[[1]]
+}
+
+
+# find_fulcrum(c(1, 2, 4, 9, 10, -10, -9, 3)) ➞ 4
+# 
+# find_fulcrum(c(9, 1, 9)) ➞ 1
+# 
+# find_fulcrum(c(7, -1, 0, -1, 1, 1, 2, 3)) ➞ 0
+# 
+# find_fulcrum(c(8, 8, 8, 8)) ➞ -1
+
+
