4d47a437467a90ba36c5c93b46ce2ab5be7cf46b
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-12-22 23:04:09 +0100

    2 done

--- day 73.R
+++ day 73.R
@@ -1,1 +1,46 @@
-#HI
+letter_check <- function(x){
+  x <- tolower(x)
+  
+  result <-strsplit(x[[2]], "")[[1]]
+  final <- strsplit(x[[1]],"")[[1]]
+  
+  isTRUE(all(result %in% final))
+}
+# letter_check(c("trances", "nectar")) ➞ True
+# 
+# letter_check(c("compadres", "DRAPES")) ➞ True
+# 
+# letter_check(c("parses", "parsecs")) ➞ False
+
+reverse.123 <- function(x){
+  result <-strsplit(x,"\\s+")[[1]]
+  soo<-lapply(result, function(k){
+    if(nchar(k) >= 5){
+      final<-strsplit(k,"")[[1]]
+      almost<- rev(final)
+      paste0(almost,collapse = "")
+    }else{
+      k
+    }
+  })
+  paste(soo, collapse = " ")
+}
+# reverse.123("Reverse") ➞ "esreveR"
+# 
+# reverse.123("This is a typical sentence.") ➞ "This is a lacipyt .ecnetnes"
+# 
+# reverse.123("The dog is big.") ➞ "The dog is big."
+
+
+x <- "This is a typical sentence."
+result <-strsplit(x,"\\s+")[[1]]
+soo<-lapply(result, function(k){
+  if(nchar(k) >= 5){
+    final<-strsplit(k,"")[[1]]
+    almost<- rev(final)
+    paste0(almost,collapse = "")
+  }else{
+    k
+  }
+})
+paste(soo, collapse = " ")
