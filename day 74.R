17e993dcc7005064ef5f0b8a71cde5e39ce00ab6
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-12-25 01:23:10 +0100

    two done

--- day 74.R
+++ day 74.R
@@ -1,5 +1,32 @@
+make_happy <- function(x) {
+  regmatches(x,gregexpr("(?<=\\:|\\;)\\(",x,perl = TRUE))[[1]] <- ")"
+  x
+}
+
+
+
 # make_happy("My current mood: :(") ➞ "My current mood: :)"
 # 
 # make_happy("I was hungry 8(") ➞ "I was hungry 8)"
 # 
 # make_happy("print('x(')") ➞ "print('x)')"
+make_happy("I'm thirsty ;(")
+
+
+count_smileys <- function(x) {
+  sum(grepl("(?<=\\:|\\;).*(\\)|D)",x,perl = TRUE))
+}
+
+# count_smileys(c(":)", ";(", ";}", ":-D")) ➞ 2
+# 
+# count_smileys(c(";D", ":-(", ":-)", ";~)")) ➞ 3
+# 
+# count_smileys(c(";]", ":[", ";*", ":$", ";-D")) ➞ 1
+
+
+
+# to_camel_case("A-B-C") ➞ "ABC"
+# 
+# to_camel_case("the-stealth-warrior") ➞ "theStealthWarrior"
+# 
+# to_camel_case("The_Stealth_Warrior") ➞ "TheStealthWarrior"
