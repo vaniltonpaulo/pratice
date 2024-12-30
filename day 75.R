151a1e5c81401fc6001dd76c3377af3b5b5c9484
vaniltonpaulo <V.Paulo@campus.lmu.de>
2024-12-30 23:09:26 +0100

    new day 75

---
+++ day 75.R
@@ -0,0 +1,5 @@
+# best_friend("he headed to the store", "h", "e") ➞ True
+# 
+# best_friend("i found an ounce with my hound", "o", "u") ➞ True
+# 
+# best_friend("we found your dynamite", "d", "y") ➞ False
