23dc2f40b98bfbd8e0c108efd3ef035e7d6faf4d
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-04 00:57:12 +0100

    Aufgabe 3 done

--- Speed up functions.R
+++ Speed up functions.R
@@ -1,5 +1,8 @@
 ### THIS DOCUMENT SERVES TO SHOW YOU HOW TO BEST SPEED UP FUNCTIONS
 
+#to analyze what makes it slow
+#this is much better
+profvis::profvis(for (i in 1:1000) ex01SimpleEcology(10, 2, 50, 1000))
 
 ########Aufgabe 1
 
@@ -230,3 +233,114 @@ ex01SimpleEcology <- function(x1, qr, g, t.max) {
 #to analyze what makes it slow
 #this is much better
 profvis::profvis(for (i in 1:1000) ex01SimpleEcology(10, 2, 50, 1000))
+
+
+
+
+
+
+
+########Aufgabe 3
+
+
+
+
+
+# Complex Ecology
+#
+# The objective here is again to rewrite the following functions to be faster
+# than the provided reference versions.
+#
+# A more complex ecological system, made up of two species, is described by the
+# "Lotka-Volterra" equations. It is made up of the "predator" and the "prey"
+# species, where the quantity of prey grows by a constant factor and gets
+# reduced proportionally to how many predators are present, and the quantity of
+# predators grows proportionally to how many prey are present and gets reduced
+# by a constant factor.
+# The actual Lotka-Volterra equations are differential equations, but here we
+# will use a discretization of the model:
+#   - prey(t + 1)     = prey(t) * qr.prey         - prey(t) * predator(t) * qi.prey
+#   - predator(t + 1) = predator(t) * qr.predator + predator(t) * prey(t) * qi.predator
+#   - When any of the equations above has a negative result, then the values
+#     are set to 0. This ensures that `prey` or `predator` never become negative.
+# We apply the constraints `qr.prey` >= 1 and 0 <= `qr.predator` <= 1 to avoid
+# either of the following scenarios: With no predators present, the prey would
+# grow exponentially, and with no prey present, the predators would die out.
+
+# Task: Calculate the sequence of quantities `prey(t)` and `predator(t)`.
+#
+# Input arguments:
+# - `x1`: `numeric(2)` indicating `prey(1)` in component 1 and `predator(1)`
+#   in component 2, both >= 0.
+# - `qr`: `numeric(2)` indicating the natural growth rates in the absence of
+#   the other species: `qr[1]` is `qr.prey` (at least 1) and `qr[2]` is
+#   `qr.predator` (between 0 and 1, inclusive).
+# - `qi`: `numeric(2)` indicating the effect of growth rates of the species on
+#   each other. `qi[1]` is `qi.prey` and `qi[2]` is `qi.predator`, both >= 0.
+# - `t.max`: positive integer `numeric(1)` indicating the number of entries in
+#   the result.
+# Return value: a `matrix` with two columns and `max.t` rows, where columns are
+# named `prey` and `predator`, containing the quantities of both at each time
+# between 1 and `max.t` (inclusive).
+# Performance goal: Your function should have a median runtime 1/2 as much as
+# `ex01ComplexEcologyReference` for values of `t.max` of 100 (less than in
+# 'Simple Ecology', so make sure to adjust the variable), `qr[1]` between 1 and
+# 1.2, `qr[2]` between 0.8 and 1, and `qi` between 0 and 0.2.
+#
+# The remarks from "simple ecology" are also relevant here.
+
+ex01ComplexEcologyReference <- function(x1, qr, qi, t.max) {
+  assertNumeric(x1, len = 2, lower = 0, any.missing = FALSE)
+  assertNumeric(qr, len = 2, any.missing = FALSE)
+  if (qr[[1]] < 1 || qr[[2]] < 0 || qr[[2]] > 1) stop("qr[1] must be at least 1 and qr[2] must be between 0 and 1.")
+  assertNumeric(qi, len = 2, lower = 0, upper = 1, any.missing = FALSE)
+  assertInt(t.max, lower = 1, tol = 1e-100)
+  result <- matrix(numeric(0), ncol = 2, dimnames = list(NULL, c("prey", "predator")))
+  xt <- x1
+  for (t in seq_len(t.max)) {
+    result <- rbind(result, xt, deparse.level = 0)
+    xt <- xt * qr + c(-1, 1) * xt[[1]] * xt[[2]] * qi
+    xt <- pmax(xt, 0)  # set values that are < 0 to 0
+  }
+  result
+}
+
+ex01ComplexEcology <- function(x1, qr, qi, t.max) {
+  #option 1 (2.25 faster)
+  assertNumeric(x1, len = 2, lower = 0, any.missing = FALSE)
+  assertNumeric(qr, len = 2, any.missing = FALSE)
+  if (qr[[1]] < 1 || qr[[2]] < 0 || qr[[2]] > 1) stop("qr[1] must be at least 1 and qr[2] must be between 0 and 1.")
+  assertNumeric(qi, len = 2, lower = 0, upper = 1, any.missing = FALSE)
+  assertInt(t.max, lower = 1, tol = 1e-100)
+  result <- matrix(numeric(0), ncol = 2, dimnames = list(NULL, c("prey", "predator")))
+  xt <- x1
+  for (t in seq_len(t.max)) {
+    result <- rbind(result, xt, deparse.level = 0)
+    xt <- xt * qr + c(-1, 1) * xt[[1]] * xt[[2]] * qi
+    #pmax was too slow
+    # set values that are < 0 to 0
+     xt[xt < 0] <- 0  
+  }
+  result
+  
+  
+  
+
+  
+  #option 2 (6.29 faster)
+  assertNumeric(x1, len = 2, lower = 0, any.missing = FALSE)
+  assertNumeric(qr, len = 2, any.missing = FALSE)
+  if (qr[[1]] < 1 || qr[[2]] < 0 || qr[[2]] > 1) stop("qr[1] must be at least 1 and qr[2] must be between 0 and 1.")
+  assertNumeric(qi, len = 2, lower = 0, upper = 1, any.missing = FALSE)
+  assertInt(t.max, lower = 1, tol = 1e-100)
+  result <- matrix(0,nrow = t.max ,ncol = 2, dimnames = list(NULL, c("prey", "predator")))
+  xt <- x1
+  for (t in seq_len(t.max)) {
+    result[t, ] <-  xt
+    xt <- xt * qr + c(-1, 1) * xt[[1]] * xt[[2]] * qi
+    #pmax was too slow
+    # set values that are < 0 to 0
+    xt[xt < 0] <- 0  
+  }
+  result
+}
