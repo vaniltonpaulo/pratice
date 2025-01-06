c80894b60f192d513cd24bc62df650b7d1f99c53
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-06 23:43:20 +0100

    Aufgabe 2 done

--- randomness.R
+++ randomness.R
@@ -90,4 +90,96 @@ ex02BootstrapCars <- function(data, replicates) {
   assertNumeric(data$peed, any.missing = FALSE)
   assertInt(replicates, lower = 5, tol = 0)
  
+  #our functions are the exact same
 }
+
+
+########Aufgabe 2
+
+
+# You are studying the effect that a specific food supplement has on the growth
+# of mice. You do this by feeding the supplement to a small number of mice and
+# measuring their weight in grams. You compare this to the weight that mice of
+# the same species have after a control diet. You reject the null hypothesis of
+# no effect if the p-value of the difference of average weights between the
+# groups is below a threshold.
+# There are not many mice in the treatment group, and there is another
+# limitation: The weight of the treatment-group is measured in integer grams,
+# Therefore, if a mouse actually weighs 10.33244524 grams, its reported value is
+# rounded to 10.
+#
+# Write a function that simulates this experiment under the null-hypothesis,
+# that the weight of the treatment group mice equals the weight of the control
+# group mice on average, but is normally distributed around that average.
+#
+# Your lab has a large number of control mice, and you therefore know the
+# average control group weight quite precisely; this is also the average
+# treatment group weight under the null hypothesis. You can therefore simulate
+# the experiment by drawing random mouse weights with a given mean and standard
+# deviation. You need to incorporate the rounding in your simulations!
+#
+# Input values:
+# - `weight.avg` : `numeric(1)` average weight of the control mice.
+# - `weight.stddev`: standard deviation of mouse weights to use.
+# - `treatment.size`: number of mice in the treatment group.
+# Return value: Vector of length `treatment.size` of (positive, integer valued)
+# simulated mouse weights.
+ex01MouseWeightSim <- function(weight.avg, weight.stdev, treatment.size) {
+  assertcoun
+  assertNumber(weight.avg)
+  assertNumber(weight.stdev)
+  assertInt(treatment.size, tol = 0, lower = 1)
+  #or
+  #assertCount(treatment.size, positive = TRUE)
+  
+  #Again nothing different from my own solution
+  result <- rnorm(treatment.size, mean = weight.avg, sd = weight.stdev)
+  #positive, integer valued --> whole numbers
+  round(result, 0)
+}
+
+# Continuation of ex01:
+#
+# Write a function that, given experimental data, calculates an approximate
+# p-value.
+#
+# Wikipedia:
+# > The p-value is the probability of obtaining test results at least as extreme
+# > as the results actually observed, under the assumption that the null
+# > hypothesis is correct.
+#
+# Do this by simulating experiments: Simulate experiments under the
+# null-hypothesis (i.e. by calling your solution for ex01). Do this
+# `simulation.rounds` times. See how often the difference between
+# `control.weight.avg` and the average of mice in a simulated experiment is
+# greater or equal to the actually observed difference between
+# `control.weight.avg` and the average of `treatment.weights`.
+#
+# This is an estimate of the `p`-value, since it counts the fraction of cases
+# under the null-hypothesis, in which the difference in averages between control
+# and treatment group is at least as much as the observed difference.
+# Input values:
+# - `control.weight.avg` : `numeric(1)` average weight of the control mice. As
+#   mentioned before, this value is known precisely.
+# - `weight.stdev`: standard deviation of mouse weights to use.
+# - `treatment.weights`: integer `numeric`: vector of weights of treatment mice.
+# - `simulation.rounds`: number of experiments to simulate.
+# Return return the estimated p-value as a `numeric(1)`.
+ex02MouseWeightPVal <- function(control.weight.avg, weight.stdev, treatment.weights, simulation.rounds) {
+  #this one was hard because of the abs and the mean
+  assertNumber(control.weight.avg)
+  assertNumber(weight.stdev)
+  assertIntegerish(treatment.weights, tol = 0, any.missing = FALSE)
+  assertInt(simulation.rounds, lower = 1, tol = 0)
+  # your code
+  
+  #Like the previous exercise we have the same solution
+  
+  result <- abs(control.weight.avg - mean(treatment.weights))
+  
+  experiment <- replicate(simulation.rounds, abs(control.weight.avg - mean(
+    ex01MouseWeightSim(control.weight.avg, weight.stdev, length(treatment.weights)))))
+  sum(experiment >= result) / simulation.rounds
+}
+
+
