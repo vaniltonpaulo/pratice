f140bcd9db5a51c38baa3c96e3ddb6bbf69d4b66
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-06 22:26:43 +0100

    new section

---
+++ randomness.R
@@ -0,0 +1,8 @@
+# hey this a file regarding Randomness
+
+ex01BrokenCoin <- function() {
+  # your code
+  coin <- c("heads", "tails", "edge")
+  
+  sample(coin, size = 1, prob = c(0.7, 0.2, 0.1))
+}
