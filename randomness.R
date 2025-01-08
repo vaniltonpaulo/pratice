42d3086839dc945310b9fb321a6af117c5f73cce
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-08 23:18:20 +0100

    day 76 new day

--- randomness.R
+++ randomness.R
@@ -271,3 +271,63 @@ ex01InferCards <- function(cards.shuffled.head, time.estimate) {
   }
   stop("nothing found")
 }
+
+
+
+
+########Aufgabe 4
+
+
+
+
+
+# Write a function that counts how often the RNG was invoked by another
+# function.
+#
+# Input:
+#  - `func` (`function`): A function that takes no arguments and returns nothing.
+#   This function will call `runif()` a varying number of times, which your own
+#   function should count.
+# Return value: An integer `numeric(1)`, indicating how often the RNG was
+# invoked by `func`. The return value should be capped at 1000, i.e. if `func()`
+# generates more than 1000 random numbers, then your function should return
+# 1000. Do this to avoid infinite loops.
+#
+# Your function can find out how often `func()` called `runif()` by saving the
+# RNG state before and after `func()` is called, then resetting the RNG state to
+# what it was in the beginning and counting how often `runif()` needs to be
+# called to get back to the end state.
+#
+# Note that `.Random.seed` is a *global* variable, so you will need to use the
+# `<<-` operator to assign to it.
+#
+# Your function should not set a seed or otherwise change the RNG state before
+# `func()` is called, and `func()` should be called only once. However, you can
+# rely on the fact that `.Random.seed` is set when your function is called.
+#
+# Example usage:
+# > ex01CountRng(function() { runif(1); runif(1) })  # returns 2
+# > ex01CountRng(function() { runif(3) })  # returns 3
+# > ex01CountRng(function() {})  # returns 0
+# > ex01CountRng(function() { runif(2000) })  # returns 1000
+ex01CountRng <- function(func) {
+  #My solution is better
+  
+  assertFunction(func)
+  # your code
+  beg <- .Random.seed
+  func()
+  enwd <- .Random.seed
+  #we had help here
+  
+  ##this part here was a problem
+  .Random.seed <<- beg
+  
+  #we have to understand this loop
+  count <- 0
+  while (!identical(.Random.seed, enwd) && count < 1000) {
+    runif(1)  # Call runif(1) to advance the RNG state
+    count <- count + 1
+  }
+  count
+}
