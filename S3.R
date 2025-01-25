285c5edd91ed308533f8ad94e5ec3b7672d9f898
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-25 22:38:52 +0100

    done

--- S3.R
+++ S3.R
@@ -134,3 +134,97 @@ dragon <- Dragon(name = "Smaug", age = 500, firepower = 100)
 
 # Test the describe function
 describe(dragon) # Calls describe.Dragon
+
+
+# Generic function
+fight <- function(x, y) {
+  UseMethod("fight")
+}
+
+# Fight method for two 'Creature' objects
+fight.Creature <- function(x, y) {
+  if (x$age > y$age) {
+    paste(x$name, "wins against", y$name, "because of age and experience.")
+  } else if (x$age < y$age) {
+    paste(y$name, "wins against", x$name, "because of age and experience.")
+  } else {
+    paste(x$name, "and", y$name, "are evenly matched!")
+  }
+}
+
+# Test the fight function
+creature1 <- Creature("Hydra", 200)
+creature2 <- Creature("Griffin", 300)
+
+fight(creature1, creature2) # Griffin wins against Hydra
+
+
+
+
+
+###############################################. complete example #########################################
+
+
+library(checkmate)
+
+# Constructor
+Creature <- function(name, age) {
+  assertString(name)
+  assertNumber(age, lower = 0)
+  
+  structure(
+    list(name = name, age = age),
+    class = "Creature"
+  )
+}
+
+# Print Method
+print.Creature <- function(x) {
+  cat("Creature Information:\n")
+  cat("  Name:", x$name, "\n")
+  cat("  Age:", x$age, "\n")
+}
+
+# Describe Method
+describe <- function(x) {
+  UseMethod("describe")
+}
+
+describe.Creature <- function(x) {
+  paste(x$name, "is", x$age, "years old.")
+}
+
+# Birthday Method
+birthday <- function(x) {
+  UseMethod("birthday")
+}
+
+birthday.Creature <- function(x) {
+  x$age <- x$age + 1
+  cat(x$name, "has turned", x$age, "years old!\n")
+  return(x)
+}
+
+# Fight Function
+fight <- function(x, y) {
+  UseMethod("fight")
+}
+
+fight.Creature <- function(x, y) {
+  if (x$age > y$age) {
+    paste(x$name, "wins against", y$name, "because of age and experience.")
+  } else if (x$age < y$age) {
+    paste(y$name, "wins against", x$name, "because of age and experience.")
+  } else {
+    paste(x$name, "and", y$name, "are evenly matched!")
+  }
+}
+
+# Test the system
+creature1 <- Creature("Hydra", 200)
+creature2 <- Creature("Griffin", 300)
+
+print(creature1)
+describe(creature2)
+fight(creature1, creature2)
+creature1 <- birthday(creature1)
