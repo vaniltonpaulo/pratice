5ffe724c95e2b4ce528444f4bfb5cec5b97ddac6
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-01-27 23:34:39 +0100

    starting hell week

---
+++ DATA-TABLE-REDO.R
@@ -0,0 +1,24 @@
+# If you have looked at some of the data.table material then this function should
+# be a breeze for you.
+
+
+# Write a function that takes a list of named lists as input and creates a `data.table` as
+# output, containing the input lists as rows.
+# Inputs:
+# - lst: `list` of named `lists` containing numeric scalars.
+# Output: `data.table`
+# The resulting `data.table` should contain the input rows in order and have columns according
+# to the names of the input list (ordering of columns does not matter).
+#
+# Example:
+# ex01List2DT(list(list(a = 1, b = 2), list(b = 3, a = 4)))
+# --> data.table(a = c(1, 4), b = c(2, 3))
+# Some lists do not contain elements for all columns; in that case, a 0 should be used:
+# ex01List2DT(list(list(a = 1, b = 2), list(a = 4, c = 5)))
+# --> data.table(a = c(1, 4), b = c(2, 0), c = c(0, 5))
+# if there are elements in the lists that are not non-NA scalar numerics, an error should be thrown.
+#
+# You should probably use `rbindlist` and possibly `nafill` / `setnafill` here.
+ex01List2DT <- function(lst) {
+  
+}
