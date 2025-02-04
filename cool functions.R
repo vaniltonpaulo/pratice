63766addaa524e47e54823c2a9778f9f654e9b1c
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-02-05 00:10:01 +0100

    nice

--- cool functions.R
+++ cool functions.R
@@ -475,4 +475,81 @@ print(makeTranspose(matrix(c("a", "b"), nrow = 1, byrow = TRUE)))
 
 
 
+orderedMatrix <-  function(x, y) {
+  matrix(1:(x * y), nrow = x, ncol = y)
+  
+}
+# orderedMatrix(5, 5) ➞  matrix(c(
+#   1, 2, 3, 4, 5,
+#   6, 7, 8, 9, 10,
+#   11, 12, 13, 14, 15,
+#   16, 17, 18, 19, 20,
+#   21, 22, 23, 24, 25
+# ), nrow = 5, byrow = TRUE)
+
+
+orderedMatrix(1, 5) 
+
+
+
+spiralMatrix <- function(n) {
+  # Create an empty n x n matrix
+  matrix_result <- matrix(0, nrow = n, ncol = n)
+  
+  # Initialize directions (right, down, left, up)
+  directions <- list(c(0, 1), c(1, 0), c(0, -1), c(-1, 0))
+  current_direction <- 1
+  
+  # Starting point
+  row <- 1
+  col <- 1
+  
+  for (value in 1:(n * n)) {
+    # Fill the current cell
+    matrix_result[row, col] <- value
+    
+    # Compute the next position
+    next_row <- row + directions[[current_direction]][1]
+    next_col <- col + directions[[current_direction]][2]
+    
+    # Check if the next position is valid
+    if (next_row < 1 || next_row > n || next_col < 1 || next_col > n || matrix_result[next_row, next_col] != 0) {
+      # Change direction clockwise
+      current_direction <- (current_direction %% 4) + 1
+      next_row <- row + directions[[current_direction]][1]
+      next_col <- col + directions[[current_direction]][2]
+    }
+    
+    # Move to the next position
+    row <- next_row
+    col <- next_col
+  }
+  
+  return(matrix_result)
+}
+
+# Test cases
+print(spiralMatrix(2))
+# Output:
+#      [,1] [,2]
+# [1,]    1    2
+# [2,]    4    3
+
+print(spiralMatrix(3))
+# Output:
+#      [,1] [,2] [,3]
+# [1,]    1    2    3
+# [2,]    8    9    4
+# [3,]    7    6    5
+
+print(spiralMatrix(4))
+# Output:
+#      [,1] [,2] [,3] [,4]
+# [1,]    1    2    3    4
+# [2,]   12   13   14    5
+# [3,]   11   16   15    6
+# [4,]   10    9    8    7
+
+
+
 
--- day 80.R
+++ day 80.R
@@ -119,3 +119,118 @@ idMtrx <- function(n) {
 # ]
 # 
 # idMtrx(0) ➞ []
+
+ 
+orderedMatrix <-  function(x, y) {
+   matrix(1:(x * y), nrow = x, ncol = y)
+  
+}
+# orderedMatrix(5, 5) ➞  matrix(c(
+#   1, 2, 3, 4, 5,
+#   6, 7, 8, 9, 10,
+#   11, 12, 13, 14, 15,
+#   16, 17, 18, 19, 20,
+#   21, 22, 23, 24, 25
+# ), nrow = 5, byrow = TRUE)
+
+
+orderedMatrix(1, 5) 
+
+
+lowerTriang <-  function(result) {
+  
+  for (i in seq_len(nrow(result))) {
+    for (j in seq_len(ncol(result))) {
+      if(i < j) {
+        result[i,j] <-  0
+      } 
+      
+    }
+    
+  }
+  
+  result
+}
+# Test cases
+print(lowerTriang(matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, byrow = TRUE)))
+# Output:
+#      [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    4    5    0
+# [3,]    7    8    9
+
+print(lowerTriang(matrix(c(5, 7, 7, 9), nrow = 2, byrow = TRUE)))
+# Output:
+#      [,1] [,2]
+# [1,]    5    0
+# [2,]    7    9
+
+print(lowerTriang(matrix(c(1, 8, 8, 1, 2, 7, 7, 2, 3, 6, 6, 3, 4, 5, 5, 4), nrow = 4, byrow = TRUE)))
+# Output:
+#      [,1] [,2] [,3] [,4]
+# [1,]    1    0    0    0
+# [2,]    2    7    0    0
+# [3,]    3    6    6    0
+# [4,]    4    5    5    4
+
+
+
+spiralMatrix <- function(n) {
+  # Create an empty n x n matrix
+  matrix_result <- matrix(0, nrow = n, ncol = n)
+  
+  # Initialize directions (right, down, left, up)
+  directions <- list(c(0, 1), c(1, 0), c(0, -1), c(-1, 0))
+  current_direction <- 1
+  
+  # Starting point
+  row <- 1
+  col <- 1
+  
+  for (value in 1:(n * n)) {
+    # Fill the current cell
+    matrix_result[row, col] <- value
+    
+    # Compute the next position
+    next_row <- row + directions[[current_direction]][1]
+    next_col <- col + directions[[current_direction]][2]
+    
+    # Check if the next position is valid
+    if (next_row < 1 || next_row > n || next_col < 1 || next_col > n || matrix_result[next_row, next_col] != 0) {
+      # Change direction clockwise
+      current_direction <- (current_direction %% 4) + 1
+      next_row <- row + directions[[current_direction]][1]
+      next_col <- col + directions[[current_direction]][2]
+    }
+    
+    # Move to the next position
+    row <- next_row
+    col <- next_col
+  }
+  
+  return(matrix_result)
+}
+
+# Test cases
+print(spiralMatrix(2))
+# Output:
+#      [,1] [,2]
+# [1,]    1    2
+# [2,]    4    3
+
+print(spiralMatrix(3))
+# Output:
+#      [,1] [,2] [,3]
+# [1,]    1    2    3
+# [2,]    8    9    4
+# [3,]    7    6    5
+
+print(spiralMatrix(4))
+# Output:
+#      [,1] [,2] [,3] [,4]
+# [1,]    1    2    3    4
+# [2,]   12   13   14    5
+# [3,]   11   16   15    6
+# [4,]   10    9    8    7
+
+
