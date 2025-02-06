b98ed7ed06626d42539b905124d47673336ff7ea
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-02-06 22:54:05 +0100

    some things

--- DATA-TABLE-REDO.R
+++ DATA-TABLE-REDO.R
@@ -242,3 +242,62 @@ ex02CleanTable <- function(data) {
   data[, (drop) := NULL][]
 
 }
+
+
+############# EXERCISE 4
+
+
+# This exercise concerns itself with data in the same format as 02_exercise_sort.R
+# Remember, the example datasets could look like the following:
+widget.corp.data <- rbindlist(list(
+  list(machine = NULL, quality = NULL, sensor01 = NULL, sensor02 = NULL, sensor03 = NULL, sensor04 = NULL),
+  list("Machine01",    78,             23,              28.6,            -23,             NA),
+  list("Machine02",    28,             41,              77.8,            NA,              27),
+  list("Machine03",    32,             57,              91.6,            -29,             10),
+  list("Machine03",    80,             NA,              32.3,            NA,              NA),
+  list("Machine03",    58,             10,              77.8,            3,               NA),
+  list("Machine02",    74,             NA,              24.5,            -18,             3),
+  list("Machine01",    46,             81,              NA,              NA,              NA),
+  list("Machine01",    24,             43,              13.3,            -22,             NA),
+  list("Machine02",    7,              96,              96.0,            0,               NA),
+  list("Machine01",    22,             107,             23.5,            7,               8),
+  list("Machine03",    98,             NA,              NA,              11,              NA)
+))
+
+# Write a function that imputes all missing values for sensor columns as the minimum of that column.
+# The function takes one `data.table` argument `data` and should return the modified `data.table`.
+# You can rely on at least one non-missing value being present in each sensor-column. The result
+# for the `widget.corp.data` example data would be
+widget.corp.data.imputed <- rbindlist(list(
+  list(machine = NULL, quality = NULL, sensor01 = NULL, sensor02 = NULL, sensor03 = NULL, sensor04 = NULL),
+  list("Machine01",    78,             23,              28.6,            -23,             3),
+  list("Machine02",    28,             41,              77.8,            -29,             27),
+  list("Machine03",    32,             57,              91.6,            -29,             10),
+  list("Machine03",    80,             10,              32.3,            -29,             3),
+  list("Machine03",    58,             10,              77.8,            3,               3),
+  list("Machine02",    74,             10,              24.5,            -18,             3),
+  list("Machine01",    46,             81,              13.3,            -29,             3),
+  list("Machine01",    24,             43,              13.3,            -22,             3),
+  list("Machine02",    7,              96,              96.0,            0,               3),
+  list("Machine01",    22,             107,             23.5,            7,               8),
+  list("Machine03",    98,             10,              13.3,            11,              3)
+))
+ex01ImputeTable <- function(data) {
+  
+  data <-  copy(widget.corp.data)
+  assertDataTable(data)
+  
+  sensorcols <- grep("sensor[0-9]+", colnames(data), value = TRUE)
+  data[, (sensorcols) := lapply(.SD, function(k) {
+    nafill(k,fill = min(k, na.rm = TRUE))
+  }), .SDcols = sensorcols][]
+  
+  #ALTERNATIVE
+  # assertDataTable(data)
+  # # your code
+  # data <- as.data.frame(data)
+  # for (i in 3:length(data)) {
+  #   data[, i][is.na(data[, i])] <- min(data[, i], na.rm = TRUE)
+  # }
+  # as.data.table(data)
+}
--- ignore.R
+++ ignore.R
@@ -1,37 +1,3 @@
-# If you have looked at some of the data.table material then this function should
-# be a breeze for you.
-
-
-# Write a function that takes a list of named lists as input and creates a `data.table` as
-# output, containing the input lists as rows.
-# Inputs:
-# - lst: `list` of named `lists` containing numeric scalars.
-# Output: `data.table`
-# The resulting `data.table` should contain the input rows in order and have columns according
-# to the names of the input list (ordering of columns does not matter).
-#
-# Example:
-# ex01List2DT(list(list(a = 1, b = 2), list(b = 3, a = 4)))
-# --> data.table(a = c(1, 4), b = c(2, 3))
-# Some lists do not contain elements for all columns; in that case, a 0 should be used:
-# ex01List2DT(list(list(a = 1, b = 2), list(a = 4, c = 5)))
-# --> data.table(a = c(1, 4), b = c(2, 0), c = c(0, 5))
-# if there are elements in the lists that are not non-NA scalar numerics, an error should be thrown.
-#
-# You should probably use `rbindlist` and possibly `nafill` / `setnafill` here.
-ex01List2DT <- function(x) {
-  assertList(x)
-  lapply(x, function(k) {
-    assertList(k, unique = TRUE)
-    lapply(k, assertNumeric)
-  })
-  
-  result <- rbindlist(x, use.names = TRUE, fill = TRUE)
-  setnafill(result, fill = 0)[]
-}
-
-
-
 widget.corp.data <- rbindlist(list(
   list(machine = NULL, quality = NULL, sensor01 = NULL, sensor02 = NULL, sensor03 = NULL, sensor04 = NULL),
   list("Machine01",    78,             23,              28.6,            -23,             NA),
@@ -47,31 +13,32 @@ widget.corp.data <- rbindlist(list(
   list("Machine03",    98,             NA,              NA,              11,              NA)
 ))
 
-
-
-
-# Write a function that accepts one `data.table` argument `data` and sorts the given data according to
-# the number of missing values in the `sensorXX` columns, in descending order. Ties should be broken
-# by the `quality`, descending. Your function should return the `data.table` with rows sorted.
-# The output for the example data would therefore be:
-widget.corp.data.sorted <- rbindlist(list(
+# Write a function that imputes all missing values for sensor columns as the minimum of that column.
+# The function takes one `data.table` argument `data` and should return the modified `data.table`.
+# You can rely on at least one non-missing value being present in each sensor-column. The result
+# for the `widget.corp.data` example data would be
+widget.corp.data.imputed <- rbindlist(list(
   list(machine = NULL, quality = NULL, sensor01 = NULL, sensor02 = NULL, sensor03 = NULL, sensor04 = NULL),
-  list("Machine03",    98,             NA,              NA,              11,              NA),
-  list("Machine03",    80,             NA,              32.3,            NA,              NA),
-  list("Machine01",    46,             81,              NA,              NA,              NA),
-  list("Machine01",    78,             23,              28.6,            -23,             NA),
-  list("Machine02",    74,             NA,              24.5,            -18,             3),
-  list("Machine03",    58,             10,              77.8,            3,               NA),
-  list("Machine02",    28,             41,              77.8,            NA,              27),
-  list("Machine01",    24,             43,              13.3,            -22,             NA),
-  list("Machine02",    7,              96,              96.0,            0,               NA),
+  list("Machine01",    78,             23,              28.6,            -23,             3),
+  list("Machine02",    28,             41,              77.8,            -29,             27),
   list("Machine03",    32,             57,              91.6,            -29,             10),
-  list("Machine01",    22,             107,             23.5,            7,               8)
+  list("Machine03",    80,             10,              32.3,            -29,             3),
+  list("Machine03",    58,             10,              77.8,            3,               3),
+  list("Machine02",    74,             10,              24.5,            -18,             3),
+  list("Machine01",    46,             81,              13.3,            -29,             3),
+  list("Machine01",    24,             43,              13.3,            -22,             3),
+  list("Machine02",    7,              96,              96.0,            0,               3),
+  list("Machine01",    22,             107,             23.5,            7,               8),
+  list("Machine03",    98,             10,              13.3,            11,              3)
 ))
-ex01SortTable <- function(data) {
+ex01ImputeTable <- function(data) {
+  
 }
 
-dt <-  copy(widget.corp.data)
+data <-  copy(widget.corp.data)
+assertDataTable(data)
 
-missings <- dt[,rowSums(is.na(.SD)), .SDcols = patterns("^sensor")]
-dt[order(missings,quality, decreasing = TRUE)]
+sensorcols <- grep("sensor[0-9]+", colnames(data), value = TRUE)
+data[, (sensorcols) := lapply(.SD, function(k) {
+  nafill(k,fill = min(k, na.rm = TRUE))
+}), .SDcols = sensorcols][]
