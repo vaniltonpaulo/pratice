939608960832b705ceda9dbaefc8e369fbda732a
vaniltonpaulo <V.Paulo@campus.lmu.de>
2025-02-07 00:23:35 +0100

    First week done. 4 weeks to go

--- DATA-TABLE-REDO.R
+++ DATA-TABLE-REDO.R
@@ -301,3 +301,173 @@ ex01ImputeTable <- function(data) {
   # }
   # as.data.table(data)
 }
+
+
+
+# You now have functions that get your data in a state fit for modelling, so you want to fit the actual models.
+#
+# For your analysis, you want to fit "linear models" using the `lm()` function that comes with `R`. If you
+# had just a `data.table` of sensor data for a single machine, for example
+widget.corp.small.example <- data.table(
+  quality = c(78, 46, 24, 22),
+  sensor02 = c(28.6, 13.3, 13.3, 23.5),
+  sensor03 = c(-23, -29, -22, 7))
+# then the linear model would be the result of calling `lm(quality ~ ., data = widget.corp.small.example)`.
+# What is happening here is that the `quality ~ .` formula tells `lm()` to model the `quality`-column against
+# all other values.
+# You want to fit a linear model for each machine, and put the resulting linear models into a `data.table` for
+# further analysis.
+#
+# Write a function that takes a `data.table` argument `data`. The function should use `ex02CleanTable()`
+# from the previous exercise (03_exercise_clean.R) to remove columns with at least 50% missing values, and then
+# `ex01ImputeTable()` to impute missing values in all remaining columns (This way you won't be fitting models on
+# columns where most data is actually just imputed).
+# The return value should be a `data.table` with columns `machine` (a `character` column, enumerating the machines
+# in any order), and `model` (a `list` column, containing models). The models should be the result of `lm()`
+# called on the data for each machine, as above.
+# The result when called with the `widget.corp.data` example dataset could be
+widget.corp.data.models <- data.table(
+  machine = c("Machine01", "Machine02", "Machine03"),
+  model = list(
+    lm(quality ~ ., data.table(
+      quality = c(78, 46, 24, 22),
+      sensor02 = c(28.6, 13.3, 13.3, 23.5),
+      sensor03 = c(-23, -29, -22, 7))),
+    lm(quality ~ ., data.table(
+      quality = c(28, 74, 7),
+      sensor02 = c(77.8, 24.5, 96),
+      sensor03 = c(-29, -18, 0))),
+    lm(quality ~ ., data.table(
+      quality = c(32, 80, 58, 98),
+      sensor02 = c(91.6, 32.3, 77.8, 13.3),
+      sensor03 = c(-29, -29, 3, 11)))
+  )
+)
+# (but the ordering of rows does not matter)
+# You might want to source the previous exercise file to make ex02CleanTable
+# available.
+ex02ModelTable <- function(data) {
+  assertDataTable(data)
+  # your code
+  data <- ex01ImputeTable(ex02CleanTable(data))
+  data[,.(model = list(lm(quality ~ ., .SD))), by = "machine"][]
+}
+
+#############.   #######################  ####################################.########## WEEK 2
+
+
+
+# A friend asked you to help him with his small corner store for various
+# items. Because his shop is both a physical store where customers can
+# take items, as well as an online shop that ships items, he has separate
+# prices for items with and without delivery to reflect the different
+# realities of the two markets.
+# Your friend changes his inventory and prices monthly, and keeps a record of all
+# sales made in that month. He wants to have a few functions that summarize
+# the sales and revenue made in a month.
+# His price-list could, for example, look like the following:
+itemshop.prices <- rbindlist(list(
+  list(item = NULL,             price.onsite = NULL, price.online = NULL),
+  list("Healing Potion",        9.99,                12.99),
+  list("Staff of Illusion",     18.95,               20.00),
+  list("Lesser Stone of Mana",  2.60,                4.00),
+  list("Greater Stone of Mana", 7.50,                9.99),
+  list("Sword of Clarity +2",   21.50,               22.99)
+))
+# (the columns are constant, but actual datasets may have more or fewer rows
+# with different items).
+# Furthermore, your friend keeps a record of items sold in a table.
+# The sales record has the following format:
+itemshop.sales <- rbindlist(list(
+  list(item = NULL,             channel = NULL),
+  list("Healing Potion",        "online"),
+  list("Sword of Clarity +2",   "onsite"),
+  list("Sword of Clarity +2",   "online"),
+  list("Sword of Clarity +2",   "onsite"),
+  list("Greater Stone of Mana", "onsite")
+))
+# (Again, actual datasets may have more or fewer rows.)
+#
+# All your functions should `assertDataTable` the input value, but do not need to make any further assertions
+# regarding the format of `data.table` arguments here.
+
+
+# Write a function that counts the number of items sold for each item type.
+# Inputs:
+# - `prices`: a `data.table` in the format of the `itemshop.prices` example
+# - `sales`: a `data.table` in the format of the `itemshop.sales` example
+# Output should be a `data.table` with columns `item` and `count`. Items
+# with no sales should appear with a count of 0. The items
+# should be in the same order as they appear in the `prices` table. The
+# output with the two example datasets would be
+itemshop.salescount <- rbindlist(list(
+  list(item = NULL,             count = NULL),
+  list("Healing Potion",        1),
+  list("Staff of Illusion",     0),
+  list("Lesser Stone of Mana",  0),
+  list("Greater Stone of Mana", 1),
+  list("Sword of Clarity +2",   3)
+))
+# You can use aggregation with `[... by ...]` here, and the special value `.N`
+# may be useful. To get the same order as the `prices` table, a join could
+# be useful.
+ex01CountSales <- function(prices, sales) {
+  # your code
+  assertDataTable(prices)
+  assertDataTable(sales)
+  
+  result <- sales[, .(count = .N), by = "item"][prices$item, on = "item"]
+  setnafill(result, fill = 0, cols = "count")[]
+}
+
+# Write a function that counts the number of items sold for each type, and
+# for each sales channel.
+# Inputs:
+# - `prices`: a `data.table` in the format of the `itemshop.prices` example
+# - `sales`: a `data.table` in the format of the `itemshop.sales` example
+# Output should be a `data.table` with columns `item`, `count.onsite`, and
+# `count.online`. Items with no sales should appear with a count of 0.
+# The items should be in the same order as they appear in the `prices` table.
+# The output with the two example datasets would be
+itemshop.saleschannel.count <- rbindlist(list(
+  list(item = NULL,             count.onsite = NULL, count.online = NULL),
+  list("Healing Potion",        0,                   1),
+  list("Staff of Illusion",     0,                   0),
+  list("Lesser Stone of Mana",  0,                   0),
+  list("Greater Stone of Mana", 1,                   0),
+  list("Sword of Clarity +2",   2,                   1)
+))
+# This can be solved with aggregation similar to ex01CountSales, but `dcast()`
+# (followed by a join) also works.
+ex02CountSalesChannels <- function(prices, sales) {
+  # your code
+  assertDataTable(prices)
+  assertDataTable(sales)
+  if (nrow(sales) == 0) {
+    return(sales[prices$item, on = "item"][, .(item, count.onsite = 0, count.online = 0)][])
+  }
+  
+  sales <- sales[, .(item, channel = paste0("count.", channel))]
+  
+  sales <- dcast(sales, item ~ channel, value.var = "channel", fun.aggregate = length)
+  sales <- rbind(sales, data.table(item = character(), count.onsite = numeric(),
+                                   #because some times there is two columns instead of three fill = TRUE is a must
+                                   count.online = numeric()), fill = TRUE)[, .(item, count.onsite, count.online)]
+  result <- sales[prices$item, on = "item"]
+  
+  setnafill(result, fill = 0, cols = c("count.onsite", "count.online"))[]
+}
+
+# Write a function that calculates the total revenue received by the shop.
+# Inputs:
+# - `prices`: a `data.table` in the format of the `itemshop.prices` example
+# - `sales`: a `data.table` in the format of the `itemshop.sales` example
+# Output: a `numeric(1)`, summing up the total revenue as given by the price
+# of each sold item for the respective channel.
+# For the example dataset, the result would be `86.48`.
+ex03Revenue <- function(prices, sales) {
+  # your code
+  assertDataTable(prices)
+  assertDataTable(sales)
+  sum(prices[sales, on = "item"][, ifelse(channel == "online", price.online, price.onsite)])
+}
